9eaadd9cfe08b64f37a12e475b97a1206db6423e
Enrique Angulo <emangulo@gmail.com>
2024-01-25 13:54:01 -0800

    Updated file to reflect updated db. Removed count as unnecessary

--- db/db.js
+++ db/db.js
@@ -11,38 +11,37 @@ async function openDb() {
 }
 
 //exportable function to insert data
-async function insertListing(count, title, price, link, dateRetrieved) {
+async function insertItem(title, price, link, dateRetrieved) {
   try {
     const db = await openDb();
 
     await db.run(
-      'INSERT OR IGNORE INTO listings(count,title,price,link,dateRetrieved) VALUES (?, ?, ?, ?, ?)',
-      count,
+      'INSERT OR IGNORE INTO job_items(title,price,link,dateRetrieved) VALUES (?, ?, ?, ?)',
       title,
       price,
       link,
       dateRetrieved
     );
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 //function to grab all rows where emailSent is 0. Return rows as an array of objects
-async function getNewListings() {
+async function getNewItems() {
   const db = await openDb();
 
-  let data = await db.all('SELECT * FROM listings WHERE emailSent = 0');
+  let data = await db.all('SELECT * FROM job_items WHERE emailSent = 0');
 
   return data;
 }
 
 async function setEmailSent(link) {
   const db = await openDb();
-  await db.run('UPDATE listings SET emailSent = 1 WHERE link = ?', link);
+  await db.run('UPDATE job_items SET emailSent = 1 WHERE link = ?', link);
 }
 
 module.exports = {
-  insertListing,
-  getNewListings,
+  insertItem,
+  getNewItems,
   setEmailSent,
 };
