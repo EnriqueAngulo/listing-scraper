f0e5f5a9aaf060f82c1317d277be461c49fda341
Enrique Angulo <emangulo@gmail.com>
2024-01-25 16:30:37 -0800

    Updated file to use models instead of single db file. Added runJobs to execute multiple jobs

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const db = require('./db/db');
 const emailer = require('./lib/emailer');
-//const fs = require('fs');
+
+//import models
+const jobModel = require('./models/job');
+const jobItemModel = require('./models/jobItem');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -17,9 +21,9 @@ const createEmailHtml = (newListing) => {
   return listing;
 };
 
-async function scrapeCraigslist() {
+async function getItemsFromPage(job, emailList) {
   const runTime = new Date();
-  const baseUrl = process.env.BASE_URL;
+  const baseUrl = job.link;
   const pagesToScrape = 3;
 
   for (let page = 0; page < pagesToScrape; page++) {
@@ -29,14 +33,20 @@ async function scrapeCraigslist() {
       const response = await axios.get(url);
       const $ = cheerio.load(response.data);
 
-      $('li').each((index, element) => {
+      $('li').each(async (_, element) => {
         const title = $(element).find('.title').text();
         const price = $(element).find('.price').text();
         const dateRetrieved = formatDate(runTime);
         const link = $(element).find('a').attr('href');
 
         if (title !== '') {
-          db.insertItem(title, price, link, dateRetrieved);
+          await jobItemModel.insertItem(
+            job.id,
+            title,
+            price,
+            link,
+            dateRetrieved
+          );
         }
       });
     } catch (error) {
@@ -44,7 +54,7 @@ async function scrapeCraigslist() {
     }
   }
 
-  const newItems = await db.getNewItems();
+  const newItems = await jobItemModel.getNewItems(job.id);
 
   if (newItems.length > 0) {
     console.log('printing new items');
@@ -53,11 +63,38 @@ async function scrapeCraigslist() {
     let emailContent = newItems.map(createEmailHtml).join('');
 
     newItems.forEach(async (newItem) => {
-      await db.setEmailSent(newItem.link);
+      await jobItemModel.setEmailSent(newItem.id);
     });
 
-    emailer.sendEmail(emailContent);
+    emailer.sendEmail(job.title, emailContent, emailList);
   }
 }
 
-scrapeCraigslist();
+async function runJobs() {
+  console.log('checking for jobs to run');
+  const jobs = await jobModel.getJobsToRun();
+
+  if (jobs.length > 0) {
+    for (const job of jobs) {
+      console.log(`running job '${job.title}'`);
+      console.log(job);
+      //get email addresses for job
+      const emails = await jobModel.getEmailsForJob(job.id);
+      let emailList = [];
+
+      for (const email of emails) {
+        emailList.push(email.email);
+      }
+      console.log('email list for job');
+      console.log(emailList);
+
+      await getItemsFromPage(job, emailList);
+
+      await jobModel.updateLastRuntime(job.id);
+    }
+  } else {
+    console.log('no jobs to run');
+  }
+}
+
+runJobs();
